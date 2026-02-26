e043959c60105e0845c07d8321d26b9e9dfd4c15
tansh6396-gif <tansh6396@gmail.com>
2026-02-26 11:19:34 +0530

    [ansh] Added UC3: Palindrome Check Using String Reverse

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -1,27 +1,30 @@
-public class Palindrome {
+
+public class Palindrome{
 
     /**
-     * Application entry point for UC4.
+     * Application entry point for UC3.
      *
      *
      */
     public static void main(String[] args) {
+
+
         String input = "radar";
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
-        boolean isPalindrome = true;
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
+
+
+        String reversed = "";
+
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
 
+        boolean isPalindrome = input.equals(reversed);
+
+
         System.out.println("Input : " + input);
+        System.out.println("Reversed : " + reversed);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
--- README.md
+++
@@ -1,6 +0,0 @@
-# PalindroneCheckerApp
-// running procedure
-// flow of project
-// objective
-// what are the topics it will be covered
-//which usercase
